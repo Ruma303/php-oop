059ac5aa4728ccd7690aeae12d3972f729ef1e27
Ruma <matteoh8@hotmail.it>
2023-08-14 09:59:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php //% Classi
+
+//$ Definizione di una classe
+
+class Persona {
+    public $nome;
+    public $cognome;
+}
+
+//$ Creazione oggetti
+/* $persona1 = new Persona();
+var_dump($persona1);
+echo '<br>';
+$persona2 = new Persona;
+var_dump($persona2);
+echo '<br>';
+$persona3 = Persona;
+var_dump($persona3); */
+
+
+
+//$ Accedere ai valori dei membri di istanza
+/* $persona1 = new Persona();
+$persona1->nome = "Alfonso";
+$persona1->cognome = "Donati";
+echo '<pre>';
+print_r($persona1);
+echo '</pre><br>'; */
+
+/* $persona1->nome = "Alessio";
+echo $persona1->nome; // Alessio */
