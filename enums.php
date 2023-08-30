32767027721519b6e9037207cdf474cbfefcbd9a
Ruma303 <matteoh8@hotmail.it>
2023-08-30 12:19:11 +0200

    aggiornamenti enums.

--- enums.php
+++ enums.php
@@ -22,30 +22,39 @@ printGreeting(Weekday::Monday); // Buona giornata e buon lavoro!
 echo '<br>';
 printGreeting(Weekday::Saturday); // É arrivato il weekend! */
 
+//var_dump(Weekday::Friday);
+
 
 //$ Definire tipo di backup
-
-
-
-/* enum Weekday: string {
-    case Monday = 'Monday';
-    case Tuesday = 'Tuesday';
-    case Wednesday = 'Wednesday';
-    case Thursday = 'Thursday';
-    case Friday = 'Friday';
-    case Saturday = 'Saturday';
-    case Sunday = 'Sunday';
+/* enum SuperHeroes: string {
+    case Hero1 = 'Superman';
+    case Hero2 = 'Batman';
+    case Hero3 = 'Spider-Man';
+    case Hero4 = 'Iron Man';
 }
+echo '<pre>'; print_r(SuperHeroes::Hero1); echo '</pre>'; */
+
+
+//* Metodi predefiniti delle enumerazioni
+/* echo SuperHeroes::Hero2->name; // Hero2
+echo SuperHeroes::Hero2->value; // Batman
+echo '<pre>'; print_r(SuperHeroes::cases()); echo '</pre>';
  */
-/* function printGreeting(Weekday $day): void {
-    if ($day === Weekday::Saturday || $day === Weekday::Sunday) {
-        echo "É arrivato il weekend!";
-    } else {
-        echo "Buona giornata e buon lavoro!";
+
+
+//* Modificare i valori degli enum
+//SuperHeroes::Hero3->value = 'Wolverine'; // Errore
+
+
+//$ Metodi nelle enumerazioni
+/* enum SuperHero: string {
+    case Hero1 = 'Superman';
+    case Hero2 = 'Batman';
+    case Hero3 = 'Spider-Man';
+    case Hero4 = 'Iron Man';
+
+    public function shoutName(): string {
+        return strtoupper($this->value);
     }
 }
-
-printGreeting(Weekday::Monday); // Buona giornata e buon lavoro!
-echo '<br>';
-printGreeting(Weekday::Saturday); // É arrivato il weekend!
- */
+echo SuperHero::Hero4->shoutName(); // IRON MAN */
